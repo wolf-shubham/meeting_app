14ce7dee5a3cdc84d13c9613ca62ba824b5d20e0
Shubham Sharma <sharma.shubham0191@gmail.com>
2022-02-15 17:11:50 +0530

    routes added

--- back-end/server.js
+++ back-end/server.js
@@ -1,12 +1,15 @@
 const express = require('express')
+const dotenv = require('dotenv')
 
 const meetings = require('./routes/meetings')
 
 const app = express()
 app.use(express.json())
+dotenv.config()
+const PORT = process.env.PORT || 3001
 
 app.use('/meeting', meetings)
 
 app.listen(3001, () => {
-    console.log('server started...')
+    console.log(`server started on PORT ${PORT}`)
 })
