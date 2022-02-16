fdad582fbdf604b9b699d2b8e62483e5d2300bca
Shubham Sharma <sharma.shubham0191@gmail.com>
2022-02-16 22:33:55 +0530

    meeting API completion

--- back-end/controllers/meetingController.js
+++ back-end/controllers/meetingController.js
@@ -21,4 +21,46 @@ const createMeeting = async (req, res) => {
 
 }
 
-module.exports = { getMeetings, createMeeting }
+const getMeetingById = async (req, res) => {
+    const meeting = await Meeting.findById(req.params.id)
+    if (meeting) {
+        return res.status(201).json(meeting)
+    }
+    else {
+        return res.status(404).json({ message: 'not found...' })
+    }
+}
+
+const updateMeeting = async (req, res) => {
+    const { title, description, category } = req.body
+    const meeting = await Meeting.findById(req.params.id)
+    if (meeting.user.toString() !== req.user._id.toString()) {
+        return res.status(403).json('you can not update others meeting')
+    }
+    if (meeting) {
+        meeting.title = title
+        meeting.description = description
+        meeting.category = category
+
+        const updatedMeeting = await meeting.save()
+        return res.status(200).json(updatedMeeting)
+    }
+    else {
+        return res.status(404).json({ message: 'not found' })
+    }
+}
+
+const deleteMeeting = async (req, res) => {
+    const meeting = await Meeting.findById(req.params.id)
+    if (meeting.user.toString() !== req.user._id.toString()) {
+        return res.status(403).json('you can not delete others meeting')
+    }
+    if (meeting) {
+        await meeting.remove()
+        return res.status(201).json({ message: 'meeting deleted.' })
+    }
+    else {
+        return res.status(404).json({ error: 'you cant delete it.' })
+    }
+}
+module.exports = { getMeetings, createMeeting, getMeetingById, updateMeeting, deleteMeeting }
--- back-end/routes/meetingRoutes.js
+++ back-end/routes/meetingRoutes.js
@@ -1,12 +1,12 @@
 const express = require('express')
-const { getMeetings, createMeeting } = require('../controllers/meetingController')
+const { getMeetings, createMeeting, getMeetingById, updateMeeting, deleteMeeting } = require('../controllers/meetingController')
 const protectedRoute = require('../middlewares/authMiddleware')
 const route = express()
 
 route.get('/', protectedRoute, getMeetings)
 route.post('/addmeeting', protectedRoute, createMeeting)
-route.get('/:id')
-route.put('/:id')
-route.delete('/:id')
+route.get('/:id', getMeetingById)
+route.put('/:id', protectedRoute, updateMeeting)
+route.delete('/:id', protectedRoute, deleteMeeting)
 
 module.exports = route
