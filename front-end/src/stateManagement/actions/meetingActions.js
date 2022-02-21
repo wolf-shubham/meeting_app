614158a5d9cd81736c9f8c50118648fd8baeca3a
Shubham Sharma <sharma.shubham0191@gmail.com>
2022-02-21 20:39:58 +0530

    create meeting state

--- front-end/src/stateManagement/actions/meetingActions.js
+++ front-end/src/stateManagement/actions/meetingActions.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-import { GET_MEETING_FAIL, GET_MEETING_REQUEST, GET_MEETING_SUCCESS } from "../constants/meetingConstant"
+import { CREATE_MEETING_FAIL, CREATE_MEETING_REQUEST, CREATE_MEETING_SUCCESS, GET_MEETING_FAIL, GET_MEETING_REQUEST, GET_MEETING_SUCCESS } from "../constants/meetingConstant"
 
 export const getMeetings = () => async (dispatch, getState) => {
     try {
@@ -21,5 +21,33 @@ export const getMeetings = () => async (dispatch, getState) => {
         const message = error
         dispatch({ type: GET_MEETING_FAIL, payload: message })
 
+    }
+}
+
+export const createMeetingActions = (title, description, category) => async (dispatch, getState) => {
+    try {
+        dispatch({
+            type: CREATE_MEETING_REQUEST
+        })
+        const { userLogin: { userInfo } } = getState()
+
+        const config = {
+            headers: {
+                "Content-Type": "application/json",
+                Authorization: `Bearer ${userInfo.token}`
+            }
+        }
+
+        const { data } = await axios.post('/meeting/addmeeting',
+            { title, description, category }, config)
+
+        dispatch({
+            type: CREATE_MEETING_SUCCESS,
+            payload: data
+        })
+    } catch (error) {
+        dispatch({
+            type: CREATE_MEETING_FAIL
+        })
     }
 }
